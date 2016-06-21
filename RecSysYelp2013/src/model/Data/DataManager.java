6dc21fb96a3bbc42b2b3b0a43e070270e78b8164
siyana <siyanaslavova@gmail.com>
2016-06-21 10:08:40 +0300

    get avr of all users/items if no information

--- RecSysYelp2013/src/model/Data/DataManager.java
+++ RecSysYelp2013/src/model/Data/DataManager.java
@@ -11,16 +11,22 @@ public class DataManager {
 	private ArrayList<DataManagerObject> users = null;
 	private ArrayList<DataManagerObject> items = null;
 
+	private float allUsersAverage = 0;
+	private float allItemsAverage = 0;
+
 	public DataManager() {
 		super();
 		ReadCSVFile reader = new ReadCSVFile("");
 		users = reader.readAverageRatings(AverageRatingsType.AverageRatingsTypeUser);
-		items = reader.readAverageRatings(AverageRatingsType.AverageRatingsTypeItem);
+		this.allUsersAverage = average(users);
+
+		items = reader.readAverageRatings(AverageRatingsType.AverageRatingsTypeItem);		
+		this.allItemsAverage = average(items);
 	}
 	/*
 	 * @category Getters and setters
 	 * */
-	
+
 	public ArrayList<DataManagerObject> getUsers() {
 		return users;
 	}
@@ -33,26 +39,34 @@ public class DataManager {
 	public void setItems(ArrayList<DataManagerObject> items) {
 		this.items = items;
 	}
-	
+
 	/*
 	 * @category Helpers
 	 * */
-	
+
+	private float average(ArrayList<DataManagerObject> set){
+		float avg = 0;
+		for (DataManagerObject dataManagerObject : set) {
+			avg += dataManagerObject.getAvgRating();
+		}
+		return avg/set.size();
+	}
+
 	public float averageStarsForUser(int userID){
 		for (DataManagerObject dataManagerObject : users) {
 			if (dataManagerObject.getObjID() == userID){
 				return dataManagerObject.getAvgRating();
 			}
 		}
-		return 0;
+		return this.allUsersAverage;
 	}
-	
+
 	public float averageStarsForItem(int itemID){
 		for (DataManagerObject dataManagerObject : items) {
 			if (dataManagerObject.getObjID() == itemID){
 				return dataManagerObject.getAvgRating();
 			}
 		}
-		return 0;
+		return this.allItemsAverage;
 	}
 }
